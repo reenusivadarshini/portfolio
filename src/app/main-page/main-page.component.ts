79f49198954a8a5013d824922b519e899a89421f
reenusivadarshini <rsd8267@gmail.com>
2024-12-31 16:34:31 +0530

    changed croll & name of pics

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,7 +3,7 @@ import { ContactComponent } from '../contact/contact.component';
 import { AboutComponent } from '../about/about.component';
 import { HomeComponent } from '../home/home.component';
 import { SkillsComponent } from '../skills/skills.component';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -13,5 +13,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  }
 
 }
